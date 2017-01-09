78c4eeac7b9c47d58d6f829beeace1664070d7a8
RaghavendraLade <lade.raghavendraece@gmail.com>
2017-01-09 12:46:01 +0530

    Second Task Extended

---
+++ Task_Jan05_Extended/JS/app/controllers/table_cntrl.js
@@ -0,0 +1,93 @@
+(function(){
+  'use strict';
+
+  angular
+    .module('innoApp')
+    .controller('tableCtrl',['$scope','paymentInfo',function($scope,paymentInfo){
+        $scope.tableInfo = {};
+      paymentInfo.dropDownValues().then(function(response){
+        var resp = angular.copy(response)
+        // var len = data[0].length;
+        // for(var i = 0; i<len; i++){
+        //   (data[0])[i].path = ((data[0])[i].path).split("\\")[2];
+        // }
+        // len = data[1].length;
+        // for(var i = 0; i<len; i++){
+        //   (data[1])[i].path = ((data[1])[i].path).split("\\")[2];
+        // }
+        var data = [];
+        var len = resp.length;
+        for(var i = 0; i< len; i++){
+          var subdata = [];
+          if(Array.isArray(resp[i])){
+            var subLen = resp[i].length;
+            for(var j = 0; j<subLen; j++){
+              subdata.push(((resp[i])[j].path).split("\\")[2]);
+            }
+
+          }
+          if(!Array.isArray(resp[i]) && typeof(resp[i]) == 'object'){
+            var subLen = resp[i].result.length;
+            for(var j = 0; j<subLen; j++){
+              subdata.push((resp[i].result)[j].value);
+            }
+          }
+          data.push(subdata);
+        }
+        $scope.data = data;
+      });
+      $scope.formChange = true;
+      $scope.generate = function(saveIt){
+        var savedInfo = angular.copy(saveIt)
+        if(savedInfo.paymentType != undefined){
+          if(!(savedInfo.paymentType in $scope.tableInfo)){
+            $scope.tableInfo[savedInfo.paymentType] = [];
+          }
+          $scope.tableInfo[savedInfo.paymentType].push(savedInfo);
+
+          $scope.info = {};
+          $scope.formChange = true;
+          $('#open-form').modal('hide');
+        }
+      }
+      $scope.editPage = true;
+      $scope.editFun = function(editData,payType,index){
+        $scope.formChange = true;
+        $scope.info = angular.copy(editData);
+        $('#open-form').modal('show');
+        $scope.editPage = false;
+        $scope.saveEditInfo = function(editedInfo){
+          ($scope.tableInfo[payType])[index] = angular.copy(editedInfo);
+          $scope.info = {};
+          $scope.formChange = true;
+          $scope.editPage = true;
+          $('#open-form').modal('hide');
+        }
+      }
+
+      $scope.delFun = function(key,index){
+        $('#del-confirm-box').modal('show');
+        $scope.confirmDel = function(){
+          ($scope.tableInfo[key]).splice(index,1);
+          $('#del-confirm-box').modal('hide');
+
+        }
+      }
+
+      $scope.dismissModal = function(){
+        $('#open-form').modal('hide');
+        $scope.info = {};
+        $scope.formChange = true;
+        $scope.editPage = true;
+      }
+
+      $scope.changeInFq = function(changed){
+        if(changed == 'Annually' || changed == 'Quarterly' || changed == 'Semi-Annually' ){
+          $scope.info.paymentDueOn = 'Specific Day of Period';
+        }
+
+      }
+    }]);
+
+
+})();
---
+++ Task_Jan05_Extended/JS/app/service/payment_ajax_service.js
@@ -0,0 +1,31 @@
+(function(){
+  'use strict';
+
+  angular
+    .module('innoApp')
+    .service('paymentInfo',function($http,$q){
+      var fileArr = ['c_frequecyType','c_paymentType','l_AccountingType','l_ChargeAmountBasis','l_GrowthType','l_PaymentDueDay','l_PaymentDueOn','l_PaymentTiming'];
+      var promiseArr = [];
+
+      function getData(url){
+        return $http.get(url)
+          .then(function(response){
+            return response.data;
+          }, function(error){
+             return $q.reject(error);
+          });
+      }
+
+      (function(){
+        var len = fileArr.length;
+        for(var i = 0; i<len; i++){
+          promiseArr.push(getData('../jsons/'+fileArr[i]+'.json'));
+        }
+      })();
+
+      this.dropDownValues = function(){
+        return $q.all(promiseArr)
+      };
+    });
+
+})();
